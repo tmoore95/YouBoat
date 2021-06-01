b99ae263c363d2569d42186555b039cd13976eac
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-06-01 15:31:22 +0100

    added seedfile for users and listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+require "open-uri"
+
+puts "removing old listings and users."
+User.destroy_all
+Listing.destroy_all
+puts "old seeds removed."
+
+5.times do
+  user = User.new(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email
+  )
+  user.save
+
+  10.times do
+    list = Listing.new(
+      name: "The #{Faker::Name.middle_name}",
+      location: Faker::Address.city,
+      craft_type: Faker::Vehicle.make,
+      price_per_day: (0..1000).to_a.sample
+    )
+    list.save
+  end
+end
+puts "listings and users added."
