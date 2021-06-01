8df42a446a13ad0b99d847d244c906276af9434b
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-06-01 12:19:00 +0100

    added validations to listings model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { minimum: 5,
+  too_short: "name must be at least 5 characters long" }
+  validates :location, presence: true
+  validates :craft_type, presence: true
+  validates :price_per_day, presence: true, numericality: { only_integer: true }
 end
