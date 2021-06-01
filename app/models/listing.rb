9bd59f868ac86f1c1065a1dbf5e30a19c165161a
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-06-01 14:31:26 +0100

    changed minimum chars for name on listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, length: { minimum: 5,
-  too_short: "name must be at least 5 characters long" }
+  validates :name, presence: true, length: { minimum: 3,
+  too_short: "name must be at least 3 characters long" }
   validates :location, presence: true
   validates :craft_type, presence: true
   validates :price_per_day, presence: true, numericality: { only_float: true }
