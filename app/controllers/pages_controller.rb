e283dae439ca8242f25f0b6636f78ea9ef56970f
hrwndavies <hrwndavies@gmail.com>
2021-06-03 10:33:21 +0100

    implemented customer bookings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,10 @@ class PagesController < ApplicationController
     @current_user = current_user
     @user = User.find(params[:id])
     @bookings = @current_user.bookings
+    c_listings = []
+    @current_user.listings.each { |l| c_listings << l.id }
+    @mbpending = Booking.all.select { |b| c_listings.include?(b.listing_id) && b.status == 'pending' }
+    @mbapproved = Booking.all.select { |b| c_listings.include?(b.listing_id) && b.status == 'approved' }
+    @mbcancelled = Booking.all.select { |b| c_listings.include?(b.listing_id) && b.status == 'cancelled' }   
   end
 end
