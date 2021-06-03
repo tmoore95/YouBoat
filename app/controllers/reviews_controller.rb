462b6874d2ed1699dd81840792d22fa76a5c970b
Tom Moore <tom_moore95@hotmail.co.uk>
2021-06-03 17:49:42 +0100

    created basic reviews function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
   def create
-    @listing = Listing.find(params[:listing_id])
+    @listing = Listing.find(params[:id])
     @review = Review.new(review_params)
     @review.listing = @listing
+    @current_user = current_user
+    @review.user_id = @current_user.id
     if @review.save
-      redirect_to listing_path(@listing)
+    redirect_to listing_path(@listing)
     else
-      render 'listings/show'
+      render "listings/new"
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :listing
-  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
-  validates :content, presence: true
 end
